3a1c532ac7f161d2e74d5d798d464685b6af9b2f
dharshib <Dharshib.8@gmail.com>
2025-06-13 18:47:33 +0530

    Add header component

---
+++ src/app/components/common/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+   return (
+      <header className="flex flex-col items-center justify-center p-3">
+         <h1 className="text-xl md:text-4xl font-bold text-center">Welcome to Uni Finder</h1>
+      </header>
+   )
+}
+
+export default Header;
